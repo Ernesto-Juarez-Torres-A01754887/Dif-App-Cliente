5d9eeca67ab7c605cc4ef480946423812dfc3d98
A01754887 Ernesto Juárez Torres <a01754887@itesm.mx>
2023-10-16 16:24:54 -0600

    Add files code

--- app/src/main/java/com/itesm/difbueno/MapaComedor.kt
+++ app/src/main/java/com/itesm/difbueno/MapaComedor.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MapaComedor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,14 @@ class MapaComedor : AppCompatActivity() {
         mapa.setOnClickListener {
             val url = "https://www.google.com/maps/d/viewer?mid=16p4XUJ3OiJqezpHleTAKGHy4Ti_8rYc&ll=19.575418665693352%2C-99.24592264703536&z=13"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
+        val floatingActionButton = findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+
+        floatingActionButton.setOnClickListener {
+            // Navega a la actividad menu_dif
+            val intent = Intent(this@MapaComedor, MenuDifActivity::class.java)
             startActivity(intent)
         }
     }
